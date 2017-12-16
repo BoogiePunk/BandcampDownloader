681d0a8da9af673d25ef341f75d5185a4373ef69
BoogiePunk <spacko306@gmail.com>
2017-12-16 14:02:04 +0100

    Init at v1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+## [1.0.0]:
+- Added everything
+
+[1.0.0]: https://github.com/BoogiePunk/BandcampDownloader/releases/tag/1.0.0
---
+++ src/main/java/com/boogiepunk/BandcampDownloader/Main.java
@@ -0,0 +1,10 @@
+package com.boogiepunk.BandcampDownloader;
+
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		BandcampDownloader.download();
+	}
+}
